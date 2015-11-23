bf447839abcbd3a9121a9020c9c0e80cac380c3e
Westley Bonack <westley.bonack@gmail.com>
2015-11-23 00:15:28 -0600

    Just need to fix the init prime additons and I think this will scale nicely

--- src/LargePrimeGenerator.java
+++ src/LargePrimeGenerator.java
@@ -32,47 +32,61 @@ class LargePrimeGenerator
     System.out.println("Integer Long: " + Integer.MAX_VALUE);
     Map<BigInteger,String> notPrimeMap = new HashMap<BigInteger,String>();
     Map<BigInteger,BigInteger> primeMap = new HashMap<BigInteger,BigInteger>();
+    Map<BigInteger,BigInteger> primeMultipleMax = new HashMap<BigInteger,BigInteger>();
 
     BigInteger lower = BigInteger.valueOf(2);
-    BigInteger upper = BigInteger.valueOf(10000);
+    BigInteger upper = BigInteger.valueOf(100);
 
-    // Only do iterations of 10000 for clarity
-    for ( ; lower.compareTo(limit) < 0 ; lower = lower.multiply(BigInteger.valueOf(2)) )
+    for ( ; lower.compareTo(limit) < 0 ; lower = upper)
     {
-      upper = lower.multiply(BigInteger.valueOf(2));
+      upper = lower.add(BigInteger.valueOf(1000));
       if (upper.compareTo(limit) >= 0)
       {
         upper = limit;
       }
 
-      System.out.println("lower");
-      System.out.println(lower);
-
-      System.out.println("upper");
-      System.out.println(upper);
+      if (primeMap.keySet().size() > 100)
+        break;
 
       Iterator<BigInteger> primesIterator = primeMap.keySet().iterator();
       while (primesIterator.hasNext())
       {
         BigInteger prime = primesIterator.next();
-        System.out.println("prime");
-        System.out.println(prime);
         BigInteger lowerPrime = prime;
-        for ( BigInteger multiple = BigInteger.valueOf(1) ; lowerPrime.compareTo(upper) < 0 ; multiple = multiple.add(BigInteger.valueOf(1)) )
+        BigInteger multiple = BigInteger.valueOf(1);
+
+        //System.out.println("prime");
+        //System.out.println(prime);
+
+        for ( ; lowerPrime.compareTo(upper) < 0 ; )
         {
-          System.out.println(lowerPrime);
           lowerPrime = lowerPrime.add(prime);
-          if (multiple.compareTo(BigInteger.valueOf(1)) > 0)
+          multiple = multiple.add(BigInteger.valueOf(1));
+          if (notPrimeMap.get(lowerPrime) == null)
+          {
             notPrimeMap.put(lowerPrime, (multiple + "," + lowerPrime));
+          }
         }
       }
 
-      int current_digit = 3;
-      for( BigInteger current = BigInteger.valueOf(3) ; current.compareTo(upper) < 0 ; )
+      int current_digit = Integer.parseInt(lower.toString().substring(lower.toString().length() - 1));
+      current_digit--;
+      BigInteger current = lower.subtract(BigInteger.valueOf(1));
+      if (current.equals(BigInteger.valueOf(1)))
+      {
+        current_digit = 2;
+        current = BigInteger.valueOf(2);
+      }
+
+      for( ; current.compareTo(upper) < 0 ; )
       {
         if (current_digit == 1 || current_digit == 3 || current_digit == 7 || current_digit == 9)
         {
-          notPrimeMap.get(current);
+          if (notPrimeMap.get(current) == null)
+          {
+            primeMap.put(current,current);
+            System.out.println(current);
+          }
 
           if (current_digit == 9)
           {
@@ -81,8 +95,18 @@ class LargePrimeGenerator
           }
           else if (current_digit == 3)
           {
-            current_digit = 1;
+            current_digit = 7;
             current = current.add(BigInteger.valueOf(4));
+          }
+          else if (current_digit == 2 || current_digit == 5)
+          {
+            current_digit = 3;
+            current = current.add(BigInteger.valueOf(1));
+          }
+          else if (current_digit == 5)
+          {
+            current_digit = 7;
+            current = current.add(BigInteger.valueOf(2));
           }
           else
           {
@@ -92,8 +116,31 @@ class LargePrimeGenerator
         }
         else
         {
-
-          System.out.println("Something is fucked up.");
+          if (current_digit == 2 && current.equals(BigInteger.valueOf(2)))
+          {
+            if (notPrimeMap.get(current) == null)
+            {
+              primeMap.put(current,current);
+              primeMap.put(current.add(BigInteger.valueOf(1)),current.add(BigInteger.valueOf(1)));
+              primeMap.put(current.add(BigInteger.valueOf(2)),current.add(BigInteger.valueOf(2)));
+              System.out.println(current);
+              System.out.println(3);
+              System.out.println(5);
+            }
+            current_digit = 3;
+            current = BigInteger.valueOf(3);
+          }
+          if (current_digit == 5 && current.equals(BigInteger.valueOf(5)))
+          {
+            System.out.println("We are in 5!!");
+            if (notPrimeMap.get(current) == null)
+            {
+              primeMap.put(current,current);
+              System.out.println(current);
+            }
+            current_digit = 7;
+            current = BigInteger.valueOf(7);
+          }
         }
       }
     }
