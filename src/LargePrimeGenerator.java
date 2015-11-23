a78e3a5010b6cf873a632a69ee7002fd2a140ef3
Westley Bonack <westley.bonack@gmail.com>
2015-11-23 00:50:31 -0600

    Got the first 100 primes working correctly

--- src/LargePrimeGenerator.java
+++ src/LargePrimeGenerator.java
@@ -32,14 +32,13 @@ class LargePrimeGenerator
     System.out.println("Integer Long: " + Integer.MAX_VALUE);
     Map<BigInteger,String> notPrimeMap = new HashMap<BigInteger,String>();
     Map<BigInteger,BigInteger> primeMap = new HashMap<BigInteger,BigInteger>();
-    Map<BigInteger,BigInteger> primeMultipleMax = new HashMap<BigInteger,BigInteger>();
 
     BigInteger lower = BigInteger.valueOf(2);
     BigInteger upper = BigInteger.valueOf(100);
 
     for ( ; lower.compareTo(limit) < 0 ; lower = upper)
     {
-      upper = lower.add(BigInteger.valueOf(1000));
+      upper = lower.multiply(BigInteger.valueOf(2));
       if (upper.compareTo(limit) >= 0)
       {
         upper = limit;
@@ -54,9 +53,6 @@ class LargePrimeGenerator
         BigInteger prime = primesIterator.next();
         BigInteger lowerPrime = prime;
         BigInteger multiple = BigInteger.valueOf(1);
-
-        //System.out.println("prime");
-        //System.out.println(prime);
 
         for ( ; lowerPrime.compareTo(upper) < 0 ; )
         {
@@ -80,9 +76,10 @@ class LargePrimeGenerator
 
       for( ; current.compareTo(upper) < 0 ; )
       {
-        if (current_digit == 1 || current_digit == 3 || current_digit == 7 || current_digit == 9)
+        if (current_digit == 1 || (current_digit == 3 && !current.equals(BigInteger.valueOf(3))) || current_digit == 7 || current_digit == 9)
         {
-          if (notPrimeMap.get(current) == null)
+
+          if (notPrimeMap.get(current) == null && primeMap.get(current) == null )
           {
             primeMap.put(current,current);
             System.out.println(current);
@@ -98,14 +95,9 @@ class LargePrimeGenerator
             current_digit = 7;
             current = current.add(BigInteger.valueOf(4));
           }
-          else if (current_digit == 2 || current_digit == 5)
+          else if (current_digit == 7)
           {
-            current_digit = 3;
-            current = current.add(BigInteger.valueOf(1));
-          }
-          else if (current_digit == 5)
-          {
-            current_digit = 7;
+            current_digit = 9;
             current = current.add(BigInteger.valueOf(2));
           }
           else
@@ -118,28 +110,41 @@ class LargePrimeGenerator
         {
           if (current_digit == 2 && current.equals(BigInteger.valueOf(2)))
           {
-            if (notPrimeMap.get(current) == null)
+            if (notPrimeMap.get(current) == null && primeMap.get(current) == null )
             {
               primeMap.put(current,current);
-              primeMap.put(current.add(BigInteger.valueOf(1)),current.add(BigInteger.valueOf(1)));
-              primeMap.put(current.add(BigInteger.valueOf(2)),current.add(BigInteger.valueOf(2)));
               System.out.println(current);
-              System.out.println(3);
-              System.out.println(5);
             }
             current_digit = 3;
             current = BigInteger.valueOf(3);
           }
-          if (current_digit == 5 && current.equals(BigInteger.valueOf(5)))
+          if (current_digit == 3 && current.equals(BigInteger.valueOf(3)))
           {
-            System.out.println("We are in 5!!");
-            if (notPrimeMap.get(current) == null)
+            if (notPrimeMap.get(current) == null && primeMap.get(current) == null )
             {
               primeMap.put(current,current);
               System.out.println(current);
             }
-            current_digit = 7;
-            current = BigInteger.valueOf(7);
+            current_digit = 5;
+            current = BigInteger.valueOf(5);
+          }
+          if (current_digit == 5)
+          {
+            if (current.equals(BigInteger.valueOf(5)))
+            {
+              if (notPrimeMap.get(current) == null && primeMap.get(current) == null )
+              {
+                primeMap.put(current,current);
+                System.out.println(current);
+              }
+              current_digit = 9;
+              current = BigInteger.valueOf(9);
+            }
+            else
+            {
+              current_digit = 7;
+              current = current.add(BigInteger.valueOf(2));
+            }
           }
         }
       }
